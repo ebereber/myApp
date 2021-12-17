86958495a75bbf6db42adf91c2e2dcc8fe488c41
Eber <eberlegza@gmail.com>
2021-12-16 21:42:56 -0300

    route

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,15 +1,18 @@
 import {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 import {getItem} from "../../Helpers/getItem"
 import ItemDetail from './ItemDetail'
 
 function ItemDetailContainer() {
 
 const [product, setProduct] = useState([])
+const {id} = useParams()
 
 useEffect(()=>{
     getItem
-        .then(data => setProduct(data))
+        .then(data => setProduct(data.find(prod => prod.id === parseInt(id))))
         .catch(err => console.log(err))
+        .finally(()=>setProduct(false))
 },[])
 
 
